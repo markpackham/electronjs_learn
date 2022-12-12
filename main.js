edcce50bee546c430449f974a5b546e77128ecd0
mark <markpackham1@gmail.com>
2022-12-12 12:54:41 +0000

    show dev tools when working in dev mode
    https://youtu.be/ML743nrkMHw?t=1092

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
+
+const isDev = process.env.NODE_ENV !== "production";
 // check if Mac is being used
 const isMac = process.platform === "darwin";
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
-    width: 500,
+    width: isDev ? 1100 : 500,
     height: 600,
   });
+
+  // Open devtools if in dev env
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
